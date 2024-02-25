bed748a51550d9861868a6f3878079e6386e00de
JPabloLobato <146821982+JPabloLobato@users.noreply.github.com>
2024-02-25 18:08:27 +0100

    Ej3 terminado

--- src/main/java/Ejercicios/Sistema_de_Alerta_y_Monitoreo.java
+++ src/main/java/Ejercicios/Sistema_de_Alerta_y_Monitoreo.java
@@ -17,4 +17,45 @@ public class Sistema_de_Alerta_y_Monitoreo {
             }
         }
     }
+    //Monitoreo constante de las variables ambientales.
+    public void MonitoreoConstante (int Umbral) {
+        for (int Variable : VariablesAmbientales) {
+            if (Variable > Umbral) {
+                System.out.println("Alerta: " + Variable + " excede el umbral de " + Umbral);
+            }
+        }
+    }
+    //Análisis de tendencias.
+    private boolean isPrime (int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    //Descomposición de problemas.
+    public void DescomposicionDeProblemas (int num) {
+        List<Integer> factores = new ArrayList<>();
+        for (int i = 2; i <= num; i++) {
+            while (num % i == 0 && isPrime(i)) {
+                factores.add(i);
+                num /= i;
+            }
+        }
+        System.out.println("Factores primos: " + factores);
+    }
+    public static void main (String[] args) {
+        int[] VariablesAmbientales = {10, 20, 30, 40, 50};
+        int[] Eventos = {5, 15, 25, 35, 45};
+        int Umbral = 25;
+        int num = 100;
+        Sistema_de_Alerta_y_Monitoreo sistema = new Sistema_de_Alerta_y_Monitoreo(VariablesAmbientales, Eventos);
+        sistema.IdentificarEventosRaros();
+        sistema.MonitoreoConstante(Umbral);
+        sistema.DescomposicionDeProblemas(num);
+    }
 }
